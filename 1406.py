3d35c068e1971dc67bf7c4b0dc93001ce4451acd
yuanrouyu <4713862@163.com>
2020-05-03 01:15:27 +0800

    first commit

---
+++ 1406.py
@@ -0,0 +1,38 @@
+#def func_a():
+ #   print("inside func_a")
+#def func_b(y):
+ #   print("inside func_b")
+  #  return y
+#def func_c(z):
+ #   print("inside func_c")
+  #  return z()
+
+#print (func_a())
+#print (5+func_b(2))
+#print (func_c(func_a))
+
+#def f(y):
+  #  x=1
+    #x+=1
+  #  print(x)
+
+#x=5
+#f(x)
+#print(x)
+
+
+#def g(y):
+    #print(x)
+    #print(x+1)
+
+#x=5
+#g(x)
+#print(x)
+
+def h(y):
+    x+=1
+x=5
+h(x)
+print(x)
+
+
---
+++ 1501.py
@@ -0,0 +1,8 @@
+def add(x,y):
+    return x+y
+def mult(x,y):
+    print(x*y)
+add(1,2)
+print(add(2,3))
+mult(3,4)
+print(mult(4,5))
